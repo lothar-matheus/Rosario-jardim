de8d596532c9b35a926f70ec86b0b2f998eeba0d
unknown <mtlemlothar@gmail.com>
2024-07-02 17:21:04 -0300

    realtime

--- rosario-jardim/src/App.js
+++ rosario-jardim/src/App.js
@@ -1,29 +1,36 @@
-
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import logo from './logo.png'
+import logo from './logo.png';
+import { database } from './dataBase';
+import { ref, onValue } from "firebase/database";
 
 function App() {
+  const [catalogo, setCatalogo] = useState({});
+
+  useEffect(() => {
+    const catalogoRef = ref(database, 'catalogo');
+    onValue(catalogoRef, (snapshot) => {
+      const data = snapshot.val();
+      setCatalogo(data);
+    });
+  }, []);
+
   return (
     <div className="App">
-      
       <header className="App-header">
-
-      <div className="container">
-      <h2>SEJA BEM-VINDO</h2>
-      <img src={logo} alt="Rosário Jardim Logo" className="logo" />
-      </div>
-       
+        <div className="container">
+          <h2>SEJA BEM-VINDO</h2>
+          <img src={logo} alt="Rosário Jardim Logo" className="logo" />
+        </div>
       </header>
       <div className='catalogo'>
-        <h2>Catálogo</h2>
-
+        <h3>Veja o nosso catálogo</h3>
         <ul>
-          <li></li>
+          {Object.keys(catalogo).map((key) => (
+            <li key={key}>{key}: {catalogo[key]}</li>
+          ))}
         </ul>
       </div>
-
-      
-     
     </div>
   );
 }
--- rosario-jardim/src/dataBase.js
+++ rosario-jardim/src/dataBase.js
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase } from "firebase/database";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDRhYOHHpT7WjVoV3Q1EwvQQH7QinLl720",
   authDomain: "rosariojardim-461dc.firebaseapp.com",
@@ -16,3 +13,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+export { database };
