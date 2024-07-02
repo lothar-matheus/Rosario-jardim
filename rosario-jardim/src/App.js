f871dcc14163afd3a2d022b88b9326d5ea179218
unknown <mtlemlothar@gmail.com>
2024-07-02 15:55:08 -0300

    basicpage

---
+++ rosario-jardim/src/App.js
@@ -0,0 +1,31 @@
+
+import './App.css';
+import logo from './logo.png'
+
+function App() {
+  return (
+    <div className="App">
+      
+      <header className="App-header">
+
+      <div className="container">
+      <h2>SEJA BEM-VINDO</h2>
+      <img src={logo} alt="Rosário Jardim Logo" className="logo" />
+      </div>
+       
+      </header>
+      <div className='catalogo'>
+        <h2>Catálogo</h2>
+
+        <ul>
+          <li></li>
+        </ul>
+      </div>
+
+      
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ rosario-jardim/src/dataBase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDRhYOHHpT7WjVoV3Q1EwvQQH7QinLl720",
+  authDomain: "rosariojardim-461dc.firebaseapp.com",
+  databaseURL: "https://rosariojardim-461dc-default-rtdb.firebaseio.com",
+  projectId: "rosariojardim-461dc",
+  storageBucket: "rosariojardim-461dc.appspot.com",
+  messagingSenderId: "724547893889",
+  appId: "1:724547893889:web:48390dd263e803ca3f6b80"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
