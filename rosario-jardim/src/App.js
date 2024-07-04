4cd9ad37a37ae1ea6cdfdfe8b12fff81647f4728
unknown <mtlemlothar@gmail.com>
2024-07-04 18:27:37 -0300

    carrinho

--- rosario-jardim/src/App.js
+++ rosario-jardim/src/App.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import logo from './logo.png';
+import logoCarrinho from './carrinhoLogo.png';
 import { database } from './dataBase';
 import { ref, onValue } from "firebase/database";
-import reportWebVitals from './reportWebVitals';
 
 function App() {
   const [catalogo, setCatalogo] = useState({});
+  const [carrinho, setCarrinho] = useState([]);
 
   useEffect(() => {
     const catalogoRef = ref(database, 'catalogo');
@@ -18,8 +19,19 @@ function App() {
   }, []);
 
   const sendWhatsAppMessage = (item, price) => {
-    const phoneNumber = "5585997448626";
+    const phoneNumber = "5585997177690";
     const message = `Olá, gostaria de comprar o ${item} por R$${price}.`;
+    const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+    window.open(url, '_blank');
+  };
+
+  const adicionarAoCarrinho = (item, price) => {
+    setCarrinho([...carrinho, { item, price }]);
+  };
+
+  const sendWhatsAppMessagePush = () => {
+    const phoneNumber = "5585997177690";
+    const message = `Olá, gostaria de comprar os seguintes itens:\n\n${carrinho.map((produto) => `${produto.item}: R$${produto.price}`).join('\n')}\n\nTotal: R$${carrinho.reduce((total, produto) => total + parseFloat(produto.price), 0)}`;
     const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
     window.open(url, '_blank');
   };
@@ -30,6 +42,7 @@ function App() {
         <div className="container">
           <h2>SEJA BEM-VINDO</h2>
           <img src={logo} alt="Rosário Jardim Logo" className="logo" />
+          <img src={logoCarrinho} alt="Carrinho Logo" className="carrinhoLogo" onClick={sendWhatsAppMessagePush} style={{ cursor: 'pointer' }} />
         </div>
       </header>
       <div className='catalogo'>
@@ -40,6 +53,7 @@ function App() {
               <li key={key}>
                 {key}: R${value}{" "}
                 <button onClick={() => sendWhatsAppMessage(key, value)}>Comprar</button>
+                <button onClick={() => adicionarAoCarrinho(key, value)}>Carrinho</button>
               </li>
             ))}
           </ul>
